58e45e23291810a00acc611909a7d61b04376d77
Maria Vilanova <maria.vilanova@upr.edu>
2020-03-09 10:16:00 -0400

    Data posting v2

--- postdata.py
+++ postdata.py
@@ -3,15 +3,11 @@ import json
 import time as t
 import queue
 
-# import dataloss_prev from dataloss
-
-def post_data(json_data, buggy_flag):
+def post_data(json_data, buggy_flag, data_queue):
 
         # url = "http://localhost:3000/api/NewBuggy/"
         url = "http://localhost:3000/api/"
         headers = {'content-type': 'application/json'} 
-
-        q = queue.Queue()
 
         ''' Receive json object '''
         ''' Receive New/Old Buggy flag'''
@@ -27,28 +23,14 @@ def post_data(json_data, buggy_flag):
 
         try:
                 r = req.getAll()  
-                if not q.empty():  
-                        for i in range(q.qsize()):
-                                d = q.get()  
+                if not data_queue.empty():  
+                        for i in range(data_queue.qsize()):
+                                d = data_queue.get()  
                                 req.post(url, data=d, headers=headers)
                 else:
                         req.post(url, data=data_dict, headers=headers) 
                         t.sleep(3)
         except Exception:
                 print("No connection! Queueing data...")
-                q.put(data_dict)
+                data_queue.put(data_dict)
                 t.sleep(3)
-
-''' ToDo: 
-          -change script to data obj (instead of json)
-          -add Everson's implementation
-'''
-
-# try:
-#         resp = req.post(url, data=data_dict, headers=headers)
-#         print(resp.status_code)
-# except:
-#         print(resp)
-
-#         # Do something (data-loss algorithm?)
-#         # dataloss_prev(data_dict, url, headers)
