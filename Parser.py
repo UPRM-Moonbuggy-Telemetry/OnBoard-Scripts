7a1f46057f2e083d711cd7dfb96daa8a3227152d
Felix M. Perez Quinones <felixperez78@hotmail.com>
2021-03-25 12:13:28 -0400

    Nah, scrap last commit lol

--- Parser.py
+++ Parser.py
@@ -19,8 +19,8 @@ def parser(Data_String, GPS_String):
     # Splits all data recieved and chops it up by its delimiter to be
     # assigned to their respective variables to be processed.
     data_types = Data_String.split(',')
-    data_types = [data_types.rstrip() for x in data_types]
-    
+    #data_types = [data_types.rstrip() for x in data_types] 
+
     """
     GPS recieved via GPIO(UART) pins
     Make calls to GPIO and receive list
